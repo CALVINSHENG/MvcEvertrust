0d15170d2e15c653a7993f31e479da6b1d00d835
CALVINSHENG <itshenq@gmail.com>
2017-01-24 22:05:11 +0800

    Add the Controller and Use the MVC 5 controller with the Entity Framework

--- MvcEvertrust/Models/OpenNorthwind.Designer.cs
+++ MvcEvertrust/Models/OpenNorthwind.Designer.cs
@@ -27,6 +27,8 @@ namespace CFNewDB.Models
         {
         }
 
+        public System.Data.Entity.DbSet<MvcEvertrust.Models.Customer> Customers { get; set; }
+
         // Add a DbSet for each entity type that you want to include in your model. For more information 
         // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
 
